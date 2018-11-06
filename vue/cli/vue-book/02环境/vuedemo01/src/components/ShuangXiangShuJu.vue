8a4a471ac77f8ae6fa3f9c77f01cccffa9e709ff
q867858321 <867858321@qq.com>
2018-11-06 10:35:09 +0800

    update

---
+++ vue/cli/vue-book/02环境/vuedemo01/src/components/ShuangXiangShuJu.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="hello">
+        a
+    </div>
+</template>
+<script>
+export default {
+    name:"ShuangXiangShuJu",
+    data:(){
+        return:{
+            msg:"aa"
+        }
+    }
+}
+</script>
+
