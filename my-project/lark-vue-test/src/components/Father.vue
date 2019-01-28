18c53855fe07090db1bca34cdee1fafe74a9eecc
q867858321 <867858321@qq.com>
2019-01-28 15:18:38 +0800

    add

---
+++ my-project/lark-vue-test/src/components/Father.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="p-father" @click="show($event,10)">father</div>
+</template>
+
+<script>
+export default {
+  name: "father",
+  data() {
+    return {
+      name: "father"
+    };
+  },
+  methods: {
+    show: function(event, name) {
+      console.log(event);
+      console.log(name);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
