c22b47b0ac3a290333855ddcba25b049bbf90e07
q867858321 <867858321@qq.com>
2019-02-17 13:57:25 +0800

    update

--- my-project/lark-vue-test/src/components/Father.vue
+++ my-project/lark-vue-test/src/components/Father.vue
@@ -1,12 +1,15 @@
 <template>
   <div id="p-father" @click="show($event,10)">father
     <router-link to="/">to helo</router-link>
+    <Son></Son>
   </div>
 </template>
 
 <script>
+import Son from "./Son.vue";
 export default {
   name: "father",
+  components: { Son },
   data() {
     return {
       name: "father"
---
+++ my-project/lark-vue-test/src/components/Son.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>子</div>
+</template>
+
+<script>
+export default {
+  name: "son",
+  data() {
+    return {};
+  },
+  mounded: function() {
+    console.log("Son", this);
+  }
+};
+</script>
+
+<style>
+</style>
