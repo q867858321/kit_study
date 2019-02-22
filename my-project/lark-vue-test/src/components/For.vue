79e78d112071fdd59d1c18f1df84d6ec7c8e1eb3
q867858321 <867858321@qq.com>
2019-02-22 13:04:11 +0800

    up

--- my-project/lark-vue-test/src/components/For.vue
+++ my-project/lark-vue-test/src/components/For.vue
@@ -1,13 +1,8 @@
 <template>
   <div>
     <ul v-if="itemList!=null && itemList.length!=0">
-      <li
-        v-for="(item,index) in itemList"
-        :key="index"
-        v-if="item!=3"
-        class="button"
-        v-bind:class="{'acitve':index==0}"
-      >
+      <li v-for="(item,index) in itemList" :key="index"
+        v-if="item!=3" class="button" v-bind:class="{'acitve':index==0}">
         {{item}}
         <span>v-bind:class="{'acitve':index==0}"</span> 跟
         <span>v-bind:class="[index==0?'active':'']"</span>
