ad422f373a8c8620fa77a8322f3105070d2ba2d8
q867858321 <867858321@qq.com>
2019-03-16 11:05:32 +0800

    update

---
+++ my-project/lark-vue-test-3.0/src/views/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>{{url}}</p>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      url: process.env.VUE_APP_URL
+    };
+  },
+  mounted: function() {
+    console.log("about", this);
+  }
+};
+</script>
+
