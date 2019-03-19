5cab370d7a1ee85d3a4416210eaae9c620924e1f
kit <867858321@qq.com>
2019-03-19 19:53:23 +0800

    update

---
+++ my-project/game/src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="m_header">
+        <div>
+            
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'header'
+}
+</script>
+
+<style>
+
+</style>
