c636fb205204a35b48e094166205fb6a789c20cc
kit <867858321@qq.com>
2019-04-04 12:09:18 +0800

    update

--- my-project/cms-admin2/cms/src/utils/clipboard.js
+++ my-project/cms-admin2/cms/src/utils/clipboard.js
@@ -6,7 +6,7 @@
 import Vue from 'vue'
 import Clipboard from 'clipboard'
 
-function clipboardSuccess() {
+function clipboardSuccess () {
   Vue.prototype.$message({
     message: 'Copy successfully',
     type: 'success',
@@ -14,28 +14,28 @@ function clipboardSuccess() {
   })
 }
 
-function clipboardError() {
+function clipboardError () {
   Vue.prototype.$message({
     message: 'Copy failed',
     type: 'error'
   })
 }
 
-export default function handleClipboard(text, event) {
+export default function handleClipboard (text, event) {
   const clipboard = new Clipboard(event.target, {
     text: () => text
-  });
+  })
   clipboard.on('success', () => {
-    clipboardSuccess();
-    clipboard.off('error');
-    clipboard.off('success');
+    clipboardSuccess()
+    clipboard.off('error')
+    clipboard.off('success')
     clipboard.destroy()
-  });
+  })
   clipboard.on('error', () => {
-    clipboardError();
-    clipboard.off('error');
-    clipboard.off('success');
+    clipboardError()
+    clipboard.off('error')
+    clipboard.off('success')
     clipboard.destroy()
-  });
+  })
   clipboard.onClick(event)
 }
