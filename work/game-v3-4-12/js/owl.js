e0d6ca2346e07c7934c0725ebafb21382b28815d
zhuqianfeng <zhuqianfeng@appfly.net>
2019-07-26 20:16:18 +0800

    update

--- work/game-v3-4-12/js/owl.js
+++ work/game-v3-4-12/js/owl.js
@@ -1,18 +1,6 @@
-$(function(){
-	var isInPhone=isInPhone();
-	if(!isInPhone){
-		$("#body").addClass("pc");
-	}else{
-		$("#body").addClass("phone");
-	}
-	
-	function isInPhone(){
-		if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
-			return true     //手机
-		} else {
-			return false       //电脑
-		}
-	}
+$(function () {
+	$(".bottomHideBtn").on("click", function () {
+		$(this).find("img").toggleClass("down");
+		$(this).next(".bottomAdsContent").slideToggle();
+	});
 })
-
-
