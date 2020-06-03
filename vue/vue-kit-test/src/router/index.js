fc089686ffb91467f5a2771024fe8d1f7e6b899f
kit <867858321@qq.com>
2020-06-03 08:58:47 +0800

    11

--- vue/vue-kit-test/src/router/index.js
+++ vue/vue-kit-test/src/router/index.js
@@ -31,121 +31,127 @@ import Layout from '@/layout'
  * all roles can be accessed
  */
 export const constantRoutes = [{
-    path: '/login',
-    component: () => import('@/views/login/index'),
-    hidden: true
-  },
-
-  {
-    path: '/404',
-    component: () => import('@/views/404'),
-    hidden: true
-  },
-
-  {
-    path: '/',
-    component: Layout,
-    redirect: '/dashboard',
-    children: [{
-      path: 'dashboard',
-      name: 'Dashboard',
-      component: () => import('@/views/dashboard/index'),
-      meta: {
-        title: 'Dashboard',
-        icon: 'dashboard'
-      }
-    }]
-  },
-
-  {
-    path: '/example',
-    component: Layout,
-    redirect: '/example/table',
-    name: 'Example',
-    meta: {
-      title: 'Example',
-      icon: 'example'
+        path: '/login',
+        component: () =>
+            import ('@/views/login/index'),
+        hidden: true
     },
-    children: [{
-        path: 'table',
-        name: 'Table',
-        component: () => import('@/views/table/index'),
+
+    {
+        path: '/404',
+        component: () =>
+            import ('@/views/404'),
+        hidden: true
+    },
+
+    {
+        path: '/',
+        component: Layout,
+        redirect: '/dashboard',
+        children: [{
+            path: 'dashboard',
+            name: 'Dashboard',
+            component: () =>
+                import ('@/views/dashboard/index'),
+            meta: {
+                title: 'Dashboard',
+                icon: 'dashboard'
+            }
+        }]
+    },
+
+    {
+        path: '/example',
+        component: Layout,
+        redirect: '/example/table',
+        name: 'Example',
         meta: {
-          title: 'Table',
-          icon: 'table'
-        }
-      },
-      {
-        path: 'tree',
-        name: 'Tree',
-        component: () => import('@/views/tree/index'),
-        meta: {
-          title: 'Tree',
-          icon: 'tree'
-        }
-      }
-    ]
-  },
+            title: 'Example',
+            icon: 'example'
+        },
+        children: [{
+                path: 'table',
+                name: 'Table',
+                component: () =>
+                    import ('@/views/table/index'),
+                meta: {
+                    title: 'Table',
+                    icon: 'table'
+                }
+            },
+            {
+                path: 'tree',
+                name: 'Tree',
+                component: () =>
+                    import ('@/views/tree/index'),
+                meta: {
+                    title: 'Tree',
+                    icon: 'tree'
+                }
+            }
+        ]
+    },
 
-  // {
-  //   path: '/form',
-  //   component: Layout,
-  //   children: [{
-  //     path: 'index',
-  //     name: 'Form',
-  //     component: () => import('@/views/form/index'),
-  //     meta: {
-  //       title: 'Form',
-  //       icon: 'form'
-  //     }
-  //   }]
-  // },
+    // {
+    //   path: '/form',
+    //   component: Layout,
+    //   children: [{
+    //     path: 'index',
+    //     name: 'Form',
+    //     component: () => import('@/views/form/index'),
+    //     meta: {
+    //       title: 'Form',
+    //       icon: 'form'
+    //     }
+    //   }]
+    // },
 
 
 
 
-  // 404 page must be placed at the end !!!
-  {
-    path: '*',
-    redirect: '/404',
-    hidden: true
-  }
+    // 404 page must be placed at the end !!!
+    {
+        path: '*',
+        redirect: '/404',
+        hidden: true
+    }
 ];
 
 export const asyncRoutes = [{
-  path: '/form',
-  component: Layout,
-  alwaysShow: true,
-  name: 'Form2',
-  meta: {
-    title: 'Form',
-    icon: 'form',
-    roles: ["admin"]
-  },
-  children: [{
-    path: 'index',
-    name: 'Form',
-    component: () => import('@/views/form/index'),
+    path: '/form',
+    component: Layout,
+    alwaysShow: true,
+    name: 'Form2',
     meta: {
-      title: 'createForm'
-    }
-  }]
+        title: 'Form',
+        icon: 'form',
+        roles: ["admin"]
+    },
+    children: [{
+        path: 'index',
+        name: 'Form',
+        component: () =>
+            import ('@/views/form/index'),
+        meta: {
+            title: 'createForm'
+        }
+    }]
 }]
 
 const createRouter = () => new Router({
-  // mode: 'history', // require service support
-  scrollBehavior: () => ({
-    y: 0
-  }),
-  routes: constantRoutes
+    mode: 'history', // require service support
+    scrollBehavior: () => ({
+        y: 0
+    }),
+    routes: constantRoutes
 })
 
 const router = createRouter()
 
 // Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
 export function resetRouter() {
-  const newRouter = createRouter()
-  router.matcher = newRouter.matcher // reset router
+    const newRouter = createRouter()
+    router.matcher = newRouter.matcher // reset router
 }
 
 export default router
