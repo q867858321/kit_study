471e41031fd21ce3ef701c07a59a665014bc86a2
q867858321 <867858321@qq.com>
2020-07-05 09:25:42 +0800

    的

--- my-project/yx/bbs/config/test.env.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-const merge = require('webpack-merge')
-const devEnv = require('./dev.env')
-
-module.exports = merge(devEnv, {
-  NODE_ENV: '"testing"',
-  Base_URL:'"http://192.168.0.173:8007/jeebbs5"'
-})
--- vue/cli/vue-book/02environment/vuetest/README.md
+++
@@ -1,9 +0,0 @@
-利用scss
-node-sass
-sass-loader
-scss
-scss-loader
-
-安装
-vue-resource
-axios
