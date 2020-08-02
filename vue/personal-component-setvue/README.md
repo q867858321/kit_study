08510ed607b02c4afb12147893963ed38d8426af
q867858321 <867858321@qq.com>
2020-08-02 16:17:14 +0800

    同步

--- vue/personal-component-setvue/README.md
+++ vue/personal-component-setvue/README.md
@@ -1,4 +1,4 @@
-# personal-component-set
+# personal-component-setvue
 
 ## Project setup
 ```
---
+++ vue/personal-component-setvue/components/index.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import TopBar from './TopBar.vue'
+const Components = {
+    TopBar
+}
+
+Object.keys(Components).forEach(name => {
+    Vue.component(name, Components[name])
+})
+
+export default Components
