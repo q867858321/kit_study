c3c25f685bff35fb2a60e8fd4aa3b3af57a195e5
zhuqianfeng <13311272940@163.com>
2020-09-15 18:15:07 +0800

    删除

--- project-on2/src/store/index.js
+++ project-on2/src/store/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import app from './modules/app'
 import getters from './getters'
-import settings from './modules/settings'
 import user from './modules/user'
 import tagsView from './modules/tagsView'
 import page from './modules/page'
@@ -11,7 +10,6 @@ Vue.use(Vuex)
 const store = new Vuex.Store({
   modules: {
     app,
-    settings,
     user,
     tagsView,
     page
